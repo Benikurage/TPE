7bdc629249435b3832aee89fe21f1bd5459f395f
Benikurage <santi.lazaro@gmail.com>
2021-11-24 17:17:03 -0300

    Agregado nuevo export de la tabla sql

--- Tienda_Videojuegos/tienda_videojuegos.sql
+++ Tienda_Videojuegos/tienda_videojuegos.sql
@@ -2,10 +2,10 @@
 -- version 5.1.0
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Oct 14, 2021 at 11:59 PM
--- Server version: 10.4.19-MariaDB
--- PHP Version: 8.0.6
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 24-11-2021 a las 21:16:12
+-- Versión del servidor: 10.4.19-MariaDB
+-- Versión de PHP: 8.0.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,13 +18,39 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `tienda_videojuegos`
+-- Base de datos: `tienda_videojuegos`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `genero`
+-- Estructura de tabla para la tabla `comentarios`
+--
+
+CREATE TABLE `comentarios` (
+  `id_comentario` int(11) NOT NULL,
+  `comentario` mediumtext NOT NULL,
+  `username` text NOT NULL,
+  `id_producto` int(11) NOT NULL,
+  `puntaje` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `comentarios`
+--
+
+INSERT INTO `comentarios` (`id_comentario`, `comentario`, `username`, `id_producto`, `puntaje`) VALUES
+(1, 'testeo', 'Santi', 60, 2),
+(4, 'Testeo desde Postman', 'Mr. Postman', 69, 5),
+(5, 'Testeo desde Postman', 'Mr. Postman', 69, 5),
+(20, 'Testeo desde Postman', 'Mr. Postman', 58, 5),
+(21, 'Testeo desde Postman', 'Mr. Postman', 58, 5),
+(26, 'Mi juego favorito!', 'Santi', 58, 5);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `genero`
 --
 
 CREATE TABLE `genero` (
@@ -33,21 +59,21 @@ CREATE TABLE `genero` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `genero`
+-- Volcado de datos para la tabla `genero`
 --
 
 INSERT INTO `genero` (`id_genero`, `genre`) VALUES
 (1, 'JRPG'),
-(2, 'Shmup'),
+(2, 'Danmaku'),
 (3, 'Retro'),
-(4, 'Sigilo'),
+(4, 'Rhythm'),
 (5, 'Deportes'),
 (6, 'Metroidvania');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `producto`
+-- Estructura de tabla para la tabla `producto`
 --
 
 CREATE TABLE `producto` (
@@ -59,7 +85,7 @@ CREATE TABLE `producto` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `producto`
+-- Volcado de datos para la tabla `producto`
 --
 
 INSERT INTO `producto` (`id_producto`, `nombre`, `descripcion`, `precio`, `id_genero`) VALUES
@@ -72,76 +98,89 @@ INSERT INTO `producto` (`id_producto`, `nombre`, `descripcion`, `precio`, `id_ge
 -- --------------------------------------------------------
 
 --
--- Table structure for table `usuario`
+-- Estructura de tabla para la tabla `usuario`
 --
 
 CREATE TABLE `usuario` (
   `id_usuario` int(11) NOT NULL,
   `nombre` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
-  `password` varchar(300) NOT NULL
+  `password` varchar(300) NOT NULL,
+  `admin` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `usuario`
+-- Volcado de datos para la tabla `usuario`
 --
 
-INSERT INTO `usuario` (`id_usuario`, `nombre`, `email`, `password`) VALUES
-(5, 'Santi uwu', 'email_falso@falso.com', '$2y$10$4SJ40MjkVQpjATNJnrz7RO1jbJ/VdA3MZsDXoOCeS3Iq9VbUQr1s6'),
-(12, 'fermin', 'fermin@fermin', '$2y$10$FGrVTx2IJJ0YCeqP728DSuLvUQsMHIQluI9MdzKI2zBC3WJFCPsz.'),
-(13, 'docente', 'docente@docente', '$2y$10$Nw.Y7mfU6ZzzlSAhZLRQTu4WOiV8Z4FJ7Pw5CqLhMuOwG2VF4F3ji');
+INSERT INTO `usuario` (`id_usuario`, `nombre`, `email`, `password`, `admin`) VALUES
+(5, 'Santi', 'email_falso@falso.com', '$2y$10$4SJ40MjkVQpjATNJnrz7RO1jbJ/VdA3MZsDXoOCeS3Iq9VbUQr1s6', 1),
+(12, 'fermin', 'fermin@fermin', '$2y$10$FGrVTx2IJJ0YCeqP728DSuLvUQsMHIQluI9MdzKI2zBC3WJFCPsz.', 1),
+(28, 'docente', 'docente@docente', '$2y$10$qy5rL3mgB9OA8hP55tOwt.ai2SMSgsaCVQBG0K4yF4OSoWNv9lTim', 1);
 
 --
--- Indexes for dumped tables
+-- Índices para tablas volcadas
 --
 
 --
--- Indexes for table `genero`
+-- Indices de la tabla `comentarios`
+--
+ALTER TABLE `comentarios`
+  ADD PRIMARY KEY (`id_comentario`);
+
+--
+-- Indices de la tabla `genero`
 --
 ALTER TABLE `genero`
   ADD PRIMARY KEY (`id_genero`);
 
 --
--- Indexes for table `producto`
+-- Indices de la tabla `producto`
 --
 ALTER TABLE `producto`
   ADD PRIMARY KEY (`id_producto`),
   ADD KEY `id_genero` (`id_genero`);
 
 --
--- Indexes for table `usuario`
+-- Indices de la tabla `usuario`
 --
 ALTER TABLE `usuario`
   ADD PRIMARY KEY (`id_usuario`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT de las tablas volcadas
 --
 
 --
--- AUTO_INCREMENT for table `genero`
+-- AUTO_INCREMENT de la tabla `comentarios`
+--
+ALTER TABLE `comentarios`
+  MODIFY `id_comentario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+
+--
+-- AUTO_INCREMENT de la tabla `genero`
 --
 ALTER TABLE `genero`
   MODIFY `id_genero` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
--- AUTO_INCREMENT for table `producto`
+-- AUTO_INCREMENT de la tabla `producto`
 --
 ALTER TABLE `producto`
-  MODIFY `id_producto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=63;
+  MODIFY `id_producto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=77;
 
 --
--- AUTO_INCREMENT for table `usuario`
+-- AUTO_INCREMENT de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 
 --
--- Constraints for dumped tables
+-- Restricciones para tablas volcadas
 --
 
 --
--- Constraints for table `producto`
+-- Filtros para la tabla `producto`
 --
 ALTER TABLE `producto`
   ADD CONSTRAINT `producto_ibfk_1` FOREIGN KEY (`id_genero`) REFERENCES `genero` (`id_genero`);
