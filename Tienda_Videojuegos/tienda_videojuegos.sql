a17a50a8b489775f1acefb96ea6f05dbb3cc88cc
Benikurage <santi.lazaro@gmail.com>
2021-11-29 01:40:47 -0300

    Actualizado export de tabla

--- Tienda_Videojuegos/tienda_videojuegos.sql
+++ Tienda_Videojuegos/tienda_videojuegos.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 24-11-2021 a las 22:00:12
+-- Tiempo de generación: 29-11-2021 a las 05:39:19
 -- Versión del servidor: 10.4.19-MariaDB
 -- Versión de PHP: 8.0.6
 
@@ -30,22 +30,19 @@ SET time_zone = "+00:00";
 CREATE TABLE `comentarios` (
   `id_comentario` int(11) NOT NULL,
   `comentario` mediumtext NOT NULL,
-  `username` text NOT NULL,
   `id_producto` int(11) NOT NULL,
-  `puntaje` int(5) NOT NULL
+  `puntaje` int(5) NOT NULL,
+  `id_usuario` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `comentarios`
 --
 
-INSERT INTO `comentarios` (`id_comentario`, `comentario`, `username`, `id_producto`, `puntaje`) VALUES
-(1, 'testeo', 'Santi', 60, 2),
-(4, 'Testeo desde Postman', 'Mr. Postman', 69, 5),
-(5, 'Testeo desde Postman', 'Mr. Postman', 69, 5),
-(20, 'Testeo desde Postman', 'Mr. Postman', 58, 5),
-(21, 'Testeo desde Postman', 'Mr. Postman', 58, 5),
-(26, 'Mi juego favorito!', 'Santi', 58, 5);
+INSERT INTO `comentarios` (`id_comentario`, `comentario`, `id_producto`, `puntaje`, `id_usuario`) VALUES
+(154, 'Se va a usar de ejemplo para el filtro!', 58, 1, 36),
+(155, 'Se va a usar de ejemplo para el filtro!', 58, 3, 36),
+(156, 'Se va a usar de ejemplo para el filtro!', 58, 5, 36);
 
 -- --------------------------------------------------------
 
@@ -64,11 +61,10 @@ CREATE TABLE `genero` (
 
 INSERT INTO `genero` (`id_genero`, `genre`) VALUES
 (1, 'JRPG'),
-(2, 'Danmaku'),
 (3, 'Retro'),
-(4, 'Rhythm'),
 (5, 'Deportes'),
-(6, 'Metroidvania');
+(6, 'Metroidvania'),
+(7, 'Sandbox');
 
 -- --------------------------------------------------------
 
@@ -91,7 +87,6 @@ CREATE TABLE `producto` (
 INSERT INTO `producto` (`id_producto`, `nombre`, `descripcion`, `precio`, `id_genero`) VALUES
 (58, 'NieR: Automata', 'Secuela de un juego que jugaron 15 personas', '2400', 1),
 (59, 'Super Mario World', 'El mejor Super Mario de la historia', '30', 3),
-(60, 'FIFA 2022', 'Mismo motor gráfico, plantel nuevo', '60', 5),
 (61, 'Super Metroid', 'Mejor Metroid', '20', 6),
 (62, 'Castlevania: Symphony of the Night', 'Gran juego de PSX', '25', 6);
 
@@ -103,7 +98,7 @@ INSERT INTO `producto` (`id_producto`, `nombre`, `descripcion`, `precio`, `id_ge
 
 CREATE TABLE `usuario` (
   `id_usuario` int(11) NOT NULL,
-  `nombre` varchar(50) NOT NULL,
+  `nombre` varchar(50) CHARACTER SET utf8mb4 NOT NULL,
   `email` varchar(50) NOT NULL,
   `password` varchar(300) NOT NULL,
   `admin` tinyint(1) NOT NULL
@@ -114,9 +109,8 @@ CREATE TABLE `usuario` (
 --
 
 INSERT INTO `usuario` (`id_usuario`, `nombre`, `email`, `password`, `admin`) VALUES
-(5, 'Santi', 'email_falso@falso.com', '$2y$10$4SJ40MjkVQpjATNJnrz7RO1jbJ/VdA3MZsDXoOCeS3Iq9VbUQr1s6', 1),
 (12, 'fermin', 'fermin@fermin', '$2y$10$FGrVTx2IJJ0YCeqP728DSuLvUQsMHIQluI9MdzKI2zBC3WJFCPsz.', 1),
-(28, 'docente', 'docente@docente', '$2y$10$qy5rL3mgB9OA8hP55tOwt.ai2SMSgsaCVQBG0K4yF4OSoWNv9lTim', 1);
+(36, 'Santi', 'email_falso@falso.com', '$2y$10$6UYsHw/TOBlOAPT5RLUTheaRfYJOLbhyrVGqSUtNwGPIEMWfAZ.Hu', 1);
 
 --
 -- Índices para tablas volcadas
@@ -126,7 +120,9 @@ INSERT INTO `usuario` (`id_usuario`, `nombre`, `email`, `password`, `admin`) VAL
 -- Indices de la tabla `comentarios`
 --
 ALTER TABLE `comentarios`
-  ADD PRIMARY KEY (`id_comentario`);
+  ADD PRIMARY KEY (`id_comentario`),
+  ADD KEY `id_producto` (`id_producto`),
+  ADD KEY `id_usuario` (`id_usuario`);
 
 --
 -- Indices de la tabla `genero`
@@ -146,7 +142,8 @@ ALTER TABLE `producto`
 --
 ALTER TABLE `usuario`
   ADD PRIMARY KEY (`id_usuario`),
-  ADD UNIQUE KEY `email` (`email`);
+  ADD UNIQUE KEY `email` (`email`),
+  ADD KEY `nombre_2` (`nombre`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
@@ -156,29 +153,36 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT de la tabla `comentarios`
 --
 ALTER TABLE `comentarios`
-  MODIFY `id_comentario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+  MODIFY `id_comentario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=168;
 
 --
 -- AUTO_INCREMENT de la tabla `genero`
 --
 ALTER TABLE `genero`
-  MODIFY `id_genero` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id_genero` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT de la tabla `producto`
 --
 ALTER TABLE `producto`
-  MODIFY `id_producto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=77;
+  MODIFY `id_producto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=86;
 
 --
 -- AUTO_INCREMENT de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
 
 --
 -- Restricciones para tablas volcadas
 --
+
+--
+-- Filtros para la tabla `comentarios`
+--
+ALTER TABLE `comentarios`
+  ADD CONSTRAINT `comentarios_ibfk_1` FOREIGN KEY (`id_producto`) REFERENCES `producto` (`id_producto`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `comentarios_ibfk_2` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Filtros para la tabla `producto`
